b616d8bb37109a51fa7d6d1fd8ea6073bf866605
Arsen Mirzayan <56628190+Arsen-Mirzayan@users.noreply.github.com>
2017-07-25 09:34:21 +0000

    Исправил функцию поиска произвольного набора данных

--- src/main/java/com/mira/jpa2/hibernate/HibernateRepository.java
+++ src/main/java/com/mira/jpa2/hibernate/HibernateRepository.java
@@ -16,209 +16,222 @@ import java.util.Map;
  */
 public class HibernateRepository implements Repository {
 
-    @PersistenceContext
-    protected HibernateEntityManager entityManager;
+  @PersistenceContext
+  protected HibernateEntityManager entityManager;
 
-    @Override
-    public <T> List<T> findAll(Class<T> cl) {
-        CriteriaQuery<T> query = createQuery(cl);
-        return find(query);
+  @Override
+  public <T> List<T> findAll(Class<T> cl) {
+    CriteriaQuery<T> query = createQuery(cl);
+    return find(query);
+  }
+
+  /**
+   * Создаёт запрос на извлечение базового класса
+   *
+   * @param cl класс
+   * @return запрос с заполненным from
+   */
+  private <T> CriteriaQuery<T> createQuery(Class<T> cl) {
+    CriteriaBuilder builder = getCriteriaBuilder();
+    CriteriaQuery<T> query = builder.createQuery(cl);
+    query.from(cl);
+    return query;
+  }
+
+  @Override
+  public <T> List<T> find(Class<T> cl, QueryBuilder<T> queryBuilder) {
+    return find(cl, queryBuilder, null);
+  }
+
+  @Override
+  public <T> List<T> find(Class<T> cl, QueryBuilder<T> queryBuilder, Integer maxResult) {
+    return find(cl, queryBuilder, 0, maxResult);
+  }
+
+  @Override
+  public <T> List<T> find(Class<T> cl, QueryBuilder<T> queryBuilder, long firstResult, Integer maxResult) {
+    CriteriaBuilder builder = getCriteriaBuilder();
+    CriteriaQuery<T> query = builder.createQuery(cl);
+    Root<T> root = query.from(cl);
+    queryBuilder.build(query, root, getCriteriaBuilder());
+    return find(query, firstResult, maxResult);
+  }
+
+  @Override
+  public <T> T find(Class<T> cl, Object id) {
+    return id != null ? entityManager.find(cl, id) : null;
+  }
+
+  @Override
+  public <T> List<T> find(CriteriaQuery<T> query) {
+    return find(query, 0, null);
+  }
+
+  @Override
+  public <T> List<T> find(CriteriaQuery<T> query, long firstResult, Integer maxResult) {
+    TypedQuery<T> typedQuery = entityManager.createQuery(query);
+    if (maxResult != null && maxResult > 0) {
+      typedQuery.setMaxResults(maxResult);
     }
-
-    /**
-     * Создаёт запрос на извлечение базового класса
-     *
-     * @param cl класс
-     * @return запрос с заполненным from
-     */
-    private <T> CriteriaQuery<T> createQuery(Class<T> cl) {
-        CriteriaBuilder builder = getCriteriaBuilder();
-        CriteriaQuery<T> query = builder.createQuery(cl);
-        query.from(cl);
-        return query;
+    if (firstResult > 0) {
+      typedQuery.setFirstResult((int) firstResult);
     }
+    return typedQuery.getResultList();
+  }
 
-    @Override
-    public <T> List<T> find(Class<T> cl, QueryBuilder<T> queryBuilder) {
-        return find(cl, queryBuilder, null);
-    }
+  @Override
+  public <T> List<T> findAnd(Class<T> cl, final Parameters<T> params) {
+    return find(cl, getWhereBuilder(params));
+  }
 
-    @Override
-    public <T> List<T> find(Class<T> cl, QueryBuilder<T> queryBuilder, Integer maxResult) {
-        CriteriaBuilder builder = getCriteriaBuilder();
-        CriteriaQuery<T> query = builder.createQuery(cl);
-        Root<T> root = query.from(cl);
-        queryBuilder.build(query, root, getCriteriaBuilder());
-        return find(query, maxResult);
-    }
+  @Override
+  public <T> List<T> findAnd(Class<T> cl, final Parameters<T> params, long firstResult, Integer maxResult) {
+    return find(cl, getWhereBuilder(params), firstResult, maxResult);
+  }
 
-    @Override
-    public <T> T find(Class<T> cl, Object id) {
-        return id != null ? entityManager.find(cl, id) : null;
-    }
-
-    @Override
-    public <T> List<T> find(CriteriaQuery<T> query) {
-        return find(query, null);
-    }
-
-    @Override
-    public <T> List<T> find(CriteriaQuery<T> query, Integer maxResult) {
-        TypedQuery<T> typedQuery = entityManager.createQuery(query);
-        if (maxResult != null && maxResult > 0) {
-            typedQuery.setMaxResults(maxResult);
+  /**
+   * Создаёт построитель условия на основе карты параметров
+   *
+   * @param params карта параметров
+   * @param <T>    класс сущности
+   * @return построитель условия
+   */
+  protected <T> QueryBuilder<T> getWhereBuilder(final Parameters<T> params) {
+    return new QueryBuilder<T>() {
+      @Override
+      protected void build() {
+        List<Predicate> predicates = new ArrayList<>(params.size());
+        for (SingularAttribute<? super T, ?> attribute : params.keySet()) {
+          Object value = params.get(attribute);
+          Path<?> path = get(attribute);
+          Predicate predicate = value != null
+              ? equal(path, value)
+              : isNull(path);
+          predicates.add(predicate);
         }
-        return typedQuery.getResultList();
+        where(predicates.toArray(new Predicate[predicates.size()]));
+      }
+    };
+  }
+
+  @Override
+  public CriteriaBuilder getCriteriaBuilder() {
+    return entityManager.getCriteriaBuilder();
+  }
+
+  @Override
+  public void saveAll(Iterable entities) {
+    for (Object entity : entities) {
+      save(entity);
     }
+  }
 
-    @Override
-    public <T> List<T> findAnd(Class<T> cl, final Parameters<T> params) {
-        return find(cl, getWhereBuilder(params));
+  @Override
+  public <T> T save(T entity) {
+    entityManager.getSession().saveOrUpdate(entity);
+    return entity;
+  }
+
+  @Override
+  public void delete(Object entity) {
+    entityManager.remove(entityManager.merge(entity));
+  }
+
+  @Override
+  public void deleteAll(Iterable entities) {
+    for (Object entity : entities) {
+      delete(entity);
     }
+  }
 
-    /**
-     * Создаёт построитель условия на основе карты параметров
-     *
-     * @param params карта параметров
-     * @param <T>    класс сущности
-     * @return построитель условия
-     */
-    protected <T> QueryBuilder<T> getWhereBuilder(final Parameters<T> params) {
-        return new QueryBuilder<T>() {
-            @Override
-            protected void build() {
-                List<Predicate> predicates = new ArrayList<>(params.size());
-                for (SingularAttribute<? super T, ?> attribute : params.keySet()) {
-                    Object value = params.get(attribute);
-                    Path<?> path = get(attribute);
-                    Predicate predicate = value != null
-                            ? equal(path, value)
-                            : isNull(path);
-                    predicates.add(predicate);
-                }
-                where(predicates.toArray(new Predicate[predicates.size()]));
-            }
-        };
+  @Override
+  public <T> long count(Class<T> cl) {
+    return count(cl, new Parameters<T>());
+  }
+
+  @Override
+  public <T> long count(Class<T> cl, QueryBuilder<T> queryBuilder) {
+    CriteriaBuilder builder = getCriteriaBuilder();
+    CriteriaQuery<Long> query = builder.createQuery(Long.class);
+    Root<T> root = query.from(cl);
+    query.select(builder.count(root));
+    queryBuilder.build(query, root, builder);
+    return entityManager.createQuery(query).getSingleResult();
+  }
+
+  @Override
+  public <T> long count(Class<T> cl, Parameters<T> params) {
+    return count(cl, getWhereBuilder(params));
+  }
+
+  @Override
+  public <T> PageResponse<T> findAll(Class<T> cl, PageRequest<T> pageRequest) {
+    return findAnd(cl, pageRequest, new Parameters<T>());
+  }
+
+  @Override
+  public <T> PageResponse<T> find(Class<T> cl, PageRequest<T> pageRequest, QueryBuilder<T> queryBuilder) {
+    CriteriaBuilder builder = getCriteriaBuilder();
+    CriteriaQuery<T> query = builder.createQuery(cl);
+    Root<T> root = query.from(cl);
+    //условие отбора
+    queryBuilder.build(query, root, builder);
+    //параметры сортировки
+    Orders<T> orders = pageRequest.getOrders();
+    List<Order> orderList = new ArrayList<>();
+    for (Map.Entry<SingularAttribute<? super T, ?>, Boolean> entry : orders) {
+      SingularAttribute<? super T, ?> key = entry.getKey();
+      Boolean direction = entry.getValue();
+      orderList.add(direction
+          ? builder.asc(root.get(key))
+          : builder.desc(root.get(key)));
     }
+    query.orderBy(orderList);
 
-    @Override
-    public CriteriaBuilder getCriteriaBuilder() {
-        return entityManager.getCriteriaBuilder();
-    }
+    //запрос на количество элементов
+    CriteriaQuery<Long> countQuery = builder.createQuery(Long.class);
+    Root<T> countRoot = countQuery.from(cl);
+    countQuery.select(builder.count(countRoot));
+    queryBuilder.build(countQuery, countRoot, builder);
 
-    @Override
-    public void saveAll(Iterable entities) {
-        for (Object entity : entities) {
-            save(entity);
-        }
-    }
-
-    @Override
-    public <T> T save(T entity) {
-        entityManager.getSession().saveOrUpdate(entity);
-        return entity;
-    }
-
-    @Override
-    public void delete(Object entity) {
-        entityManager.remove(entityManager.merge(entity));
-    }
-
-    @Override
-    public void deleteAll(Iterable entities) {
-        for (Object entity : entities) {
-            delete(entity);
-        }
-    }
-
-    @Override
-    public <T> long count(Class<T> cl) {
-        return count(cl, new Parameters<T>());
-    }
-
-    @Override
-    public <T> long count(Class<T> cl, QueryBuilder<T> queryBuilder) {
-        CriteriaBuilder builder = getCriteriaBuilder();
-        CriteriaQuery<Long> query = builder.createQuery(Long.class);
-        Root<T> root = query.from(cl);
-        query.select(builder.count(root));
-        queryBuilder.build(query, root, builder);
-        return entityManager.createQuery(query).getSingleResult();
-    }
-
-    @Override
-    public <T> long count(Class<T> cl, Parameters<T> params) {
-        return count(cl, getWhereBuilder(params));
-    }
-
-    @Override
-    public <T> PageResponse<T> findAll(Class<T> cl, PageRequest<T> pageRequest) {
-        return findAnd(cl, pageRequest, new Parameters<T>());
-    }
-
-    @Override
-    public <T> PageResponse<T> find(Class<T> cl, PageRequest<T> pageRequest, QueryBuilder<T> queryBuilder) {
-        CriteriaBuilder builder = getCriteriaBuilder();
-        CriteriaQuery<T> query = builder.createQuery(cl);
-        Root<T> root = query.from(cl);
-        //условие отбора
-        queryBuilder.build(query, root, builder);
-        //параметры сортировки
-        Orders<T> orders = pageRequest.getOrders();
-        List<Order> orderList = new ArrayList<>();
-        for (Map.Entry<SingularAttribute<? super T, ?>, Boolean> entry : orders) {
-            SingularAttribute<? super T, ?> key = entry.getKey();
-            Boolean direction = entry.getValue();
-            orderList.add(direction
-                    ? builder.asc(root.get(key))
-                    : builder.desc(root.get(key)));
-        }
-        query.orderBy(orderList);
-
-        //запрос на количество элементов
-        CriteriaQuery<Long> countQuery = builder.createQuery(Long.class);
-        Root<T> countRoot = countQuery.from(cl);
-        countQuery.select(builder.count(countRoot));
-        queryBuilder.build(countQuery, countRoot, builder);
-
-        return find(query, countQuery, pageRequest);
-    }
-
-    /**
-     * Выполняет запрос с постраничным извлечением данных
-     *
-     * @param query       запрос
-     * @param pageRequest запрос на постраничное извлечение данных
-     * @param <T>         тип извлекаемых данных
-     * @return страницу
-     */
-    protected <T> PageResponse<T> find(CriteriaQuery<T> query, CriteriaQuery<Long> countQuery, PageRequest<T> pageRequest) {
-        TypedQuery<T> typedQuery = entityManager.createQuery(query);
-        typedQuery.setFirstResult((int) pageRequest.getStartPosition());
-        long pageSize = pageRequest.getPageSize();
-        typedQuery.setMaxResults((int) pageSize);
-        List<T> result = typedQuery.getResultList();
-
-        long count = entityManager.createQuery(countQuery).getSingleResult();
-        long pageCount = (count != 0) ? (long) Math.ceil(count / (float) pageSize) : 0;
+    return find(query, countQuery, pageRequest);
+  }
 
 
-        return new PageResponse<T>(result, pageRequest.getPage(), pageCount, count);
-    }
+  /**
+   * Выполняет запрос с постраничным извлечением данных
+   *
+   * @param query       запрос
+   * @param pageRequest запрос на постраничное извлечение данных
+   * @param <T>         тип извлекаемых данных
+   * @return страницу
+   */
+  protected <T> PageResponse<T> find(CriteriaQuery<T> query, CriteriaQuery<Long> countQuery, PageRequest<T> pageRequest) {
+    TypedQuery<T> typedQuery = entityManager.createQuery(query);
+    typedQuery.setFirstResult((int) pageRequest.getStartPosition());
+    long pageSize = pageRequest.getPageSize();
+    typedQuery.setMaxResults((int) pageSize);
+    List<T> result = typedQuery.getResultList();
+
+    long count = entityManager.createQuery(countQuery).getSingleResult();
+    long pageCount = (count != 0) ? (long) Math.ceil(count / (float) pageSize) : 0;
 
 
-    @Override
-    public <T> PageResponse<T> findAnd(Class<T> cl, PageRequest<T> pageRequest, Parameters<T> params) {
-        return find(cl, pageRequest, getWhereBuilder(params));
-    }
+    return new PageResponse<T>(result, pageRequest.getPage(), pageCount, count);
+  }
 
-    @Override
-    public void flush() {
-        entityManager.flush();
-    }
+  @Override
+  public <T> PageResponse<T> findAnd(Class<T> cl, PageRequest<T> pageRequest, Parameters<T> params) {
+    return find(cl, pageRequest, getWhereBuilder(params));
+  }
 
-    @Override
-    public <T> TypedQuery<T> createQuery(CriteriaQuery<T> criteriaQuery) {
-        return entityManager.createQuery(criteriaQuery);
-    }
+  @Override
+  public void flush() {
+    entityManager.flush();
+  }
+
+  @Override
+  public <T> TypedQuery<T> createQuery(CriteriaQuery<T> criteriaQuery) {
+    return entityManager.createQuery(criteriaQuery);
+  }
 }
--- src/main/java/com/mira/jpa2/service/DefaultDalService.java
+++ src/main/java/com/mira/jpa2/service/DefaultDalService.java
@@ -4,8 +4,10 @@ import com.mira.jpa2.*;
 import com.mira.jpa2.data.DefaultPersistentObject;
 import com.mira.jpa2.data.DefaultPersistentObject_;
 
+import javax.persistence.criteria.Predicate;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -14,81 +16,97 @@ import java.util.List;
  */
 public abstract class DefaultDalService<E extends DefaultPersistentObject> extends AbstractService<E, Long> {
 
-    /**
-     * Ищет произвольную страницу подходящую под указанные параметры
-     *
-     * @param pageSize количество записей на странице
-     * @return произвольная страница с результатами
-     */
-    public PageResponse<E> findRandom(long pageSize) {
-        return findRandom(new Parameters<>(), pageSize);
-    }
-
-    /**
-     * Ищет произвольную страницу подходящую под указанные параметры
-     *
-     * @param parameters параметры запроса
-     * @param pageSize   количество записей на странице
-     * @return произвольная страница с результатами
-     */
-    public PageResponse<E> findRandom(Parameters<E> parameters, long pageSize) {
-        long count = repository.count(getEntityClass(), parameters);
-        long pageCount = count / pageSize + (count % pageSize == 0 ? 0 : 1);
-        long page = (long) Math.floor(Math.random() * pageCount);
-        Orders<E> orders = new Orders<>(DefaultPersistentObject_.id);
-        return findAnd(parameters, new PageRequest<>(page, pageSize, orders));
-    }
-
-    /**
-     * @return итератор, проходящий по всем элементам списка
-     */
-    public Iterable<E> iterate() {
-        return iterate(new QueryBuilder<E>() {
-            @Override
-            protected void build() {
-
-            }
-        });
-    }
-
-    /**
-     * Создаёт итератор, который проходит по полному результату запроса. Данные из базы выгружаются постранично.
-     *
-     * @param queryBuilder запрос
-     * @return итератор
-     */
-    public Iterable<E> iterate(QueryBuilder<E> queryBuilder) {
-        return () -> new QueryResultIterator(queryBuilder);
-    }
-
-    /**
-     * Итератор, ходящий по результатам запроса
-     */
-    private class QueryResultIterator implements Iterator<E> {
-        private List<E> buffer = new ArrayList<>();
-        private int index = 0;
-        private long page = -1;
-        private long bufferSize = 1000;
-        private QueryBuilder<E> queryBuilder;
-
-        public QueryResultIterator(QueryBuilder<E> queryBuilder) {
-            this.queryBuilder = queryBuilder;
-        }
-
+  /**
+   * Ищет произвольную страницу подходящую под указанные параметры
+   *
+   * @param count количество записей на странице
+   * @return произвольная страница с результатами
+   */
+  public List<E> findRandom(long count) {
+    List<E> result = new LinkedList<>();
+    long total = repository.count(getEntityClass());
+    while (total > 0 && result.size() < count) {
+      long startPosition = (long) Math.floor(total * Math.random());
+      result.add(repository.find(getEntityClass(), new QueryBuilder<E>() {
         @Override
-        public boolean hasNext() {
-            if (buffer.size() <= index) {
-                page++;
-                PageResponse<E> response = DefaultDalService.this.find(queryBuilder, new PageRequest<E>(page, bufferSize, new Orders<E>(DefaultPersistentObject_.id)));
-                buffer = response.getResult();
-                index = 0;
-            }
-            return !buffer.isEmpty();
+        protected void build() {
+          Predicate where = null;
+          for (E object : result) {
+            where = and(where, not(equal(get(DefaultPersistentObject_.id), object.getId())));
+          }
+          if (where != null) {
+            where(where);
+          }
         }
-
-        @Override
-        public E next() {
-            return buffer.get(index++);
-        }
+      }, startPosition, 1).get(0));
+      total--;
     }
+    return result;
+  }
+
+  /**
+   * Возвращает список из произвольных элементов
+   *
+   * @param parameters параметры запроса
+   * @param count      количество
+   * @return список произвольных элементов
+   */
+  public List<E> findRandom(Parameters<E> parameters, long count) {
+    long total = repository.count(getEntityClass(), parameters);
+    long startPosition = total <= count ? 0 : (long) Math.floor((total - count) * Math.random());
+    return repository.findAnd(getEntityClass(), parameters, startPosition, (int) count);
+  }
+
+  /**
+   * @return итератор, проходящий по всем элементам списка
+   */
+  public Iterable<E> iterate() {
+    return iterate(new QueryBuilder<E>() {
+      @Override
+      protected void build() {
+
+      }
+    });
+  }
+
+  /**
+   * Создаёт итератор, который проходит по полному результату запроса. Данные из базы выгружаются постранично.
+   *
+   * @param queryBuilder запрос
+   * @return итератор
+   */
+  public Iterable<E> iterate(QueryBuilder<E> queryBuilder) {
+    return () -> new QueryResultIterator(queryBuilder);
+  }
+
+  /**
+   * Итератор, ходящий по результатам запроса
+   */
+  private class QueryResultIterator implements Iterator<E> {
+    private List<E> buffer = new ArrayList<>();
+    private int index = 0;
+    private long page = -1;
+    private long bufferSize = 1000;
+    private QueryBuilder<E> queryBuilder;
+
+    public QueryResultIterator(QueryBuilder<E> queryBuilder) {
+      this.queryBuilder = queryBuilder;
+    }
+
+    @Override
+    public boolean hasNext() {
+      if (buffer.size() <= index) {
+        page++;
+        PageResponse<E> response = DefaultDalService.this.find(queryBuilder, new PageRequest<E>(page, bufferSize, new Orders<E>(DefaultPersistentObject_.id)));
+        buffer = response.getResult();
+        index = 0;
+      }
+      return !buffer.isEmpty();
+    }
+
+    @Override
+    public E next() {
+      return buffer.get(index++);
+    }
+  }
 }
